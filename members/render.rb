757261eb3567ab0931cf9415713aad78401e2e4e
Matt Neary <neary.matt@gmail.com>
2014-05-04 15:25:52 -0400

    links to profiles corrected

--- members/render.rb
+++ members/render.rb
@@ -7,13 +7,13 @@ members = JSON.parse(
 puts "# Members"
 puts "This page reflects the data in `members/members.json` and
       can be rebuilt by running `make` from the project root. Ruby,
-      RubyGems, and the `json` gem are dependencies."
+      RubyGems, and the `json` gem are dependencies of the rendering script."
 puts "<table>"
 puts "  <tr><td></td><td>Name</td><td>Languages</td><td>Platforms</td></tr>"
 members.each {|member|
   puts "  <tr>
     <td><img src='#{member['avatar']}?s=50' width='50px'></td>
-    <td><a href='#{member['github']}'>#{member['name']}</a></td>
+    <td><a href='http://github.com/#{member['github']}'>#{member['name']}</a></td>
     <td>#{member['languages'].join(', ')}</td>
     <td>#{member['platforms'].join(', ')}</td>
   </tr>" 
